ae4f8a8016b203c84ab24c5aa57d94742f71a9b9
Sachayanthan <sachayanthan.v.1999@gmail.com>
2022-12-23 13:10:43 +0530

    second commit

--- file1.js
+++ file1.js
@@ -1,11 +1,2 @@
 console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
-console.log("This is the first line for js");
+console.log("sachayanthan");
